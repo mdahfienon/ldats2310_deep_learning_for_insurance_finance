a1e1ea7ffaec7637fa46c6f15f88b106e1368d13
mdahfienon <mfienon@yahoo.com>
2023-10-19 21:29:12 +0200

    before working

---
+++ deep_learning_project.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 13 14:22:31 2023
+
+@author: MATHIAS
+"""
+
+#%% libraries
+
+import numpy as np
+import pandas as pd
+
+
+#%% reading data
+
+#dt = pd.read_csv('Car Insurance Claim.csv' )
+
+dt1 = pd.read_excel("Car Insurance Claim.xlsx")
+
+# %% data cleaning
